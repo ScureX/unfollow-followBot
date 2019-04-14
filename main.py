203115a5023f6638a05792070d92ec1a3ad672f8
ScureX <47725553+ScureX@users.noreply.github.com>
2019-04-14 17:21:03 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -12,38 +12,52 @@ api = tweepy.API(auth)
 
 #######
 
-day = datetime.datetime.today().weekday()
-hour = datetime.datetime.now().hour
-minute = datetime.datetime.now(). minute
-time = str(hour)+":"+str(minute)
-
-
 follow = ["2dartonline", "yourpantsu", "pomfclub", "nisopict_bot_k2", "nisopict_bot_kr", "regike_", "rocksylight", "cuteanimegirls_", "acecatgirlbot", "gifsanime_"]
 
 ########
 
+while True:
+    if datetime.datetime.today().weekday() <= 0 and day >= 5:
+        print(str(datetime.datetime.now().hour)+":"+str(datetime.datetime.now().minute))
+        
+        if str(datetime.datetime.now().hour)+":"+str(datetime.datetime.now().minute)=="7:0":
+            for a in follow:
+                api.destroy_friendship(a)
+                time.sleep(1)
+                time.sleep(30)
+        elif zeit=="16:0":
+            for a in follow:
+                api.create_friendship(a)
+                time.sleep(1)
+        else:
+            print("not the right time")
+            time.sleep(5)
+            time.sleep(20)
+            
+    else:
+        print("weekend")
+        time.sleep(100)
 
-cmd = input("f/uf: ")
-
-if cmd=="f":
-    l = len(follow)
-    print(l)
-    for a in follow:
-        api.create_friendship(a)
-        l = l-1
-        print(l)
-        time.sleep(1)
-elif cmd=="uf":
-    l = len(follow)
-    print(l)
-    for b in follow:
-        api.destroy_friendship(b)
-        l = l-1
-        print(l)
-        time.sleep(1)
-else:
-    print("command doesn't exist")
 
 
-  
 
+#cmd = input("f/uf: ")
+
+#if cmd=="f":
+#    sec = len(follow)
+#    print(sec)
+#    for a in follow:
+#        api.create_friendship(a)
+#        sec = sec-1
+#        print(sec)
+#        time.sleep(1)
+#elif cmd=="uf":
+#    sec = len(follow)
+#    print(sec)
+#    for b in follow:
+#        api.destroy_friendship(b)
+#        sec = sec-1
+#        print(l)
+#        time.sleep(1)
+#else:
+#    print("command doesn't exist")
