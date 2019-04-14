e1fd67ef4d14ab50f985f6980cb9f0c4ac4b8af7
ScureX <47725553+ScureX@users.noreply.github.com>
2019-04-14 16:10:18 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -10,35 +10,40 @@ auth = tweepy.OAuthHandler(tc.CONSUMER_KEY, tc.CONSUMER_SECRET)
 auth.set_access_token(tc.ACCESS_TOKEN, tc.ACCESS_TOKEN_SECRET)
 api = tweepy.API(auth)
 
+#######
+
+day = datetime.datetime.today().weekday()
+hour = datetime.datetime.now().hour
+minute = datetime.datetime.now(). minute
+time = str(hour)+":"+str(minute)
 
 
-follow = ["2dartonline", "yourpantsu", "pomfclub", "nisopict_bot_k2", "nisopict_bot_kr", "regike_", "rocksylight", "cuteanimegirls_", "acecatgirlbot"]
+follow = ["2dartonline", "yourpantsu", "pomfclub", "nisopict_bot_k2", "nisopict_bot_kr", "regike_", "rocksylight", "cuteanimegirls_", "acecatgirlbot", "gifsanime_"]
+
+########
+
 
 cmd = input("f/uf: ")
 
 if cmd=="f":
+    l = len(follow)
+    print(l)
     for a in follow:
         api.create_friendship(a)
+        l = l-1
+        print(l)
         time.sleep(1)
 elif cmd=="uf":
+    l = len(follow)
+    print(l)
     for b in follow:
         api.destroy_friendship(b)
+        l = l-1
+        print(l)
         time.sleep(1)
 else:
     print("command doesn't exist")
 
 
-hour = datetime.datetime.now().hour
-minute = datetime.datetime.now(). minute
-
-time = str(hour)+":"+str(minute)
-
-#print(str(hour)+":"+str(minute))
-
-
-
-#print(time)
-#if time=="11:29":
-   # print("now")
-   
+  
 
