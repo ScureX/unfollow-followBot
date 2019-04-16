69f70fe6392c5d69a5b576dd15ef6549a6d189ae
ScureX <47725553+ScureX@users.noreply.github.com>
2019-04-16 21:11:16 +0200

    Create twitter_credentials.py

---
+++ twitter_credentials.py
@@ -0,0 +1,7 @@
+ACCESS_TOKEN = ""
+ACCESS_TOKEN_SECRET = ""
+CONSUMER_KEY = ""
+CONSUMER_SECRET = ""
+
+#Copy and paste the tokens from your twitter app into here. #
+#Make sure that you dont leave spaces as this can result in a failure to authenticate you!#
